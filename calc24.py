fecf089395415b8ddfdf7a9e81ce88db4d617cd8
Steven Chen <megakilo@gmail.com>
2018-01-16 23:51:03 -0800

    update Python version

--- calc24.py
+++ calc24.py
@@ -1,6 +1,5 @@
 #! /usr/bin/env python3
 
-import itertools
 import random
 import sys
 
@@ -8,33 +7,47 @@ MIN_PYTHON = (3, 0)
 if sys.version_info < MIN_PYTHON:
     sys.exit("Python %s.%s or later is required.\n" % MIN_PYTHON)
 
-def compute(numbers, target):
-    assert numbers
-    if len(numbers) == 1:
-        if target == numbers[0]["value"]:
-            print("Solution: " + numbers[0]["expr"])
-            return True
-        else:
-            return False
-    pick_two = itertools.permutations(range(len(numbers)), 2)
-    for index in pick_two:
-        rest = [numbers[i] for i in range(len(numbers)) if i not in index]
-        idx1, idx2 = index
-        for infix in ["+", "-", "*", "/"]:
-            if infix == "/" and numbers[idx2]["value"] == 0:
-                continue
-            expr = "(%s%s%s)" % (numbers[idx1]["expr"], infix, numbers[idx2]["expr"])
-            value = eval("(%s%s%s)" % (numbers[idx1]["value"], infix, numbers[idx2]["value"]))
-            if value < 0 or value < 1:
-                continue
-            if compute(rest+[{"value":value, "expr":expr}], target) is True:
-                return True
-    return False
+def combine2(nums):
+    result = []
+    result.append((nums[0][0] + nums[1][0], "({} + {})".format(nums[0][1], nums[1][1])))
+    result.append((nums[0][0] * nums[1][0], "({} * {})".format(nums[0][1], nums[1][1])))
+    if nums[0][0] > nums[1][0]:
+        result.append((nums[0][0] - nums[1][0], "({} - {})".format(nums[0][1], nums[1][1])))
+    else:
+        result.append((nums[1][0] - nums[0][0], "({} - {})".format(nums[1][1], nums[0][1])))
+    if nums[1][0] != 0:
+        result.append((nums[0][0] / nums[1][0], "({} / {})".format(nums[0][1], nums[1][1])))
+    if nums[0][0] != 0:
+        result.append((nums[1][0] / nums[0][0], "({} / {})".format(nums[1][1], nums[0][1])))
+    return result
+
+def split(nums, n):
+    if n == 0:
+        return [([], nums)]
+    if len(nums) <= n:
+        return [(nums, [])]
+    head = nums[0]
+    tail = nums[1:]
+    return [(v[0], v[1] + [head]) for v in split(tail, n)] + [(v[0] + [head], v[1]) for v in split(tail, n-1)]
+
+def reduce(nums):
+    result = []
+    for pair in split(nums, 2):
+        for r in combine2(pair[0]):
+            result.append(pair[1] + [r])
+    return result
+
+def calc(nums):
+    if len(nums) == 1:
+        return nums
+    result = []
+    for xs in reduce(nums):
+        result.extend(calc(xs))
+    return result
 
 if __name__ == '__main__':
-    random_numbers = [random.randint(1, 13) for x in range(4)]
-    print(random_numbers)
-    print("="*len(str(random_numbers)))
-    processed_numbers = [{"value":x, "expr":str(x)} for x in random_numbers]
-    if compute(processed_numbers, 24) is False:
-        print("No Solution")
+    for c in range(100):
+        vector = [random.randint(1, 13) for x in range(4)]
+        numbers = [(float(x), str(x)) for x in vector]
+        solution = next((x[1] for x in calc(numbers) if x[0] == 24), "No Solution")
+        print("{} -> {}".format(vector, solution))
