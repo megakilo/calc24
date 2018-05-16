3c936ea826ae87a3802568642b201ed4339ff95b
Steven Chen <megakilo@gmail.com>
2018-05-15 23:22:01 -0700

    Update the Haskell implementation

--- calc24.hs
+++ calc24.hs
@@ -11,40 +11,35 @@ main =
     nums <- randomList 4
     putStr $ show (map (round . fst) nums) ++ " -> "
     case find (\x -> fst x == 24) $ calc nums of
-      Just n -> print $ snd n
-      Nothing -> print "No Solution"
-
-createNumber :: Integer -> Number
-createNumber x = (fromInteger x, show x)
+      Just n -> putStrLn $ snd n
+      Nothing -> putStrLn "No Solution"
 
 randomList :: Int -> IO [Number]
 randomList n = do
   nums <- replicateM n $ randomRIO (1, 13 :: Integer)
   return $ map createNumber nums
+  where
+    createNumber x = (fromInteger x, show x)
 
 calc :: [Number] -> [Number]
 calc xs
   | length xs == 1 = xs
   | otherwise = reduce xs >>= calc
+  where
+    reduce xs =
+      split xs 2 >>=
+      (\([num1, num2], nontaken) -> [r : nontaken | r <- combine num1 num2])
 
-reduce :: [Number] -> [[Number]]
-reduce xs =
-  split2 xs 2 >>= (\(taken, nontaken) -> [r : nontaken | r <- combine2 taken])
-
-split2 :: [a] -> Int -> [([a], [a])]
-split2 xs 0 = [([], xs)]
-split2 xs n
+split :: [a] -> Int -> [([a], [a])]
+split xs 0 = [([], xs)]
+split xs n
   | length xs <= n = [(xs, [])]
   | otherwise =
-    map
-      (\pair -> (fst pair, head xs : snd pair))
-      (split2 (tail xs) n) ++
-    map
-      (\pair -> (head xs : fst pair, snd pair))
-      (split2 (tail xs) (n - 1))
+    map (\pair -> (fst pair, head xs : snd pair)) (split (tail xs) n) ++
+    map (\pair -> (head xs : fst pair, snd pair)) (split (tail xs) (n - 1))
 
-combine2 :: [Number] -> [Number]
-combine2 [(x, xstr), (y, ystr)] =
+combine :: Number -> Number -> [Number]
+combine (x, xstr) (y, ystr) =
   [ (x + y, printf "(%s + %s)" xstr ystr)
   , (x * y, printf "(%s * %s)" xstr ystr)
   , (x - y, printf "(%s - %s)" xstr ystr)
@@ -52,4 +47,3 @@ combine2 [(x, xstr), (y, ystr)] =
   , (x / y, printf "(%s / %s)" xstr ystr)
   , (y / x, printf "(%s / %s)" ystr xstr)
   ]
-combine2 _ = []
