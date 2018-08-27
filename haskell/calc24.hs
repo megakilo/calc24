d33237241dbb7d27001e203a1694ee0a92a67d12
Steven Chen <megakilo@gmail.com>
2018-08-26 17:43:58 -0700

    Improve Haskell version
    Use datatype for better clarification
    Conditionally add parentheses to expressions

--- haskell/calc24.hs
+++ haskell/calc24.hs
@@ -3,23 +3,25 @@ import Data.List (find)
 import System.Random (randomRIO)
 import Text.Printf (printf)
 
-type Number = (Float, String)
+data Number = Number
+  { value :: Float
+  , expression :: String
+  , operator :: Char
+  }
 
 main :: IO ()
 main =
   replicateM_ 1000 $ do
     nums <- randomList 4
-    let challenge = show $ map (round . fst) nums
-    case find (\x -> fst x == 24) $ calc nums of
-      Just n -> putStrLn (challenge ++ " -> " ++ snd n)
+    let challenge = show $ map (round . value) nums
+    case find (\num -> value num == 24) $ calc nums of
+      Just num -> putStrLn (challenge ++ " -> " ++ expression num)
       Nothing -> putStrLn (challenge ++ " -> No Solution")
 
 randomList :: Int -> IO [Number]
 randomList n = do
   nums <- replicateM n $ randomRIO (1, 13 :: Integer)
-  return $ map createNumber nums
-  where
-    createNumber x = (fromInteger x, show x)
+  return $ map (\x -> Number (fromInteger x) (show x) 'X') nums
 
 calc :: [Number] -> [Number]
 calc xs
@@ -37,12 +39,16 @@ split xs n
     map (\pair -> (head xs : fst pair, snd pair)) (split (tail xs) (n - 1))
 
 combine :: Number -> Number -> [Number]
-combine (x, xstr) (y, ystr) =
-  [ (x + y, printf "(%s + %s)" xstr ystr)
-  , (x * y, printf "(%s * %s)" xstr ystr)
-  , (x / y, printf "(%s / %s)" xstr ystr)
-  , (y / x, printf "(%s / %s)" ystr xstr)
+combine (Number x xExpr xOp) (Number y yExpr yOp) =
+  [ Number (x + y) (printf "%s + %s" xExpr yExpr) '+'
+  , Number (x * y) (printf "%s * %s" (addParentheses xExpr xOp "+-") (addParentheses yExpr yOp "+-")) '*'
+  , Number (x / y) (printf "%s / %s" (addParentheses xExpr xOp "+-") (addParentheses yExpr yOp "+-*/")) '/'
+  , Number (y / x) (printf "%s / %s" (addParentheses yExpr yOp "+-") (addParentheses xExpr xOp "+-*/")) '/'
   , if x > y
-      then (x - y, printf "(%s - %s)" xstr ystr)
-      else (y - x, printf "(%s - %s)" ystr xstr)
+      then Number (x - y) (printf "%s - %s" xExpr (addParentheses yExpr yOp "+-")) '-'
+      else Number (y - x) (printf "%s - %s" yExpr (addParentheses xExpr xOp "+-")) '-'
   ]
+  where
+    addParentheses expr op conditions
+      | op `elem` conditions = "(" ++ expr ++ ")"
+      | otherwise = expr
