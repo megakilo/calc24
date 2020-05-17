1ac864fce01eadf3294a13d262f2625eaa074e88
Steven Chen <megakilo@gmail.com>
2020-05-17 14:06:05 -0700

    Use unique and unowned pointer to improve performance.

--- cpp/calc24.cpp
+++ cpp/calc24.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <memory>
+#include <optional>
 #include <random>
 #include <string>
 #include <vector>
@@ -8,11 +9,11 @@ enum class OpType { None, Plus, Minus, Multiply, Divide };
 
 struct Number {
   float value;
-  std::shared_ptr<Number> left;
-  std::shared_ptr<Number> right;
+  Number* left;
+  Number* right;
   OpType op;
-  Number(const float v) : value(v), op(OpType::None) {}
-  Number(std::shared_ptr<Number> num1, std::shared_ptr<Number> num2,
+  explicit Number(const float v) : value(v), op(OpType::None) {}
+  Number(Number* num1, Number* num2,
          const OpType current_op) {
     op = current_op;
     left = num1;
@@ -37,9 +38,9 @@ struct Number {
   }
 };
 
-std::string print(std::shared_ptr<Number> num);
+std::string print(Number* num);
 
-std::string create_expr(std::shared_ptr<Number> num,
+std::string create_expr(Number* num,
                         const bool is_denominator) {
   std::string s;
   if (num->op == OpType::Plus || num->op == OpType::Minus ||
@@ -48,7 +49,7 @@ std::string create_expr(std::shared_ptr<Number> num,
   return s.append(print(num));
 }
 
-std::string print(std::shared_ptr<Number> num) {
+std::string print(Number* num) {
   std::string s;
   switch (num->op) {
     case OpType::None:
@@ -76,37 +77,36 @@ std::string print(std::shared_ptr<Number> num) {
   return s;
 }
 
-std::vector<std::shared_ptr<Number>> combine(std::shared_ptr<Number> num1,
-                                             std::shared_ptr<Number> num2) {
-  std::vector<std::shared_ptr<Number>> result;
-  result.push_back(std::make_shared<Number>(num1, num2, OpType::Plus));
-  result.push_back(std::make_shared<Number>(num1, num2, OpType::Multiply));
+std::vector<std::unique_ptr<Number>> combine(Number* num1, Number* num2) {
+  std::vector<std::unique_ptr<Number>> result;
+  result.push_back(std::make_unique<Number>(num1, num2, OpType::Plus));
+  result.push_back(std::make_unique<Number>(num1, num2, OpType::Multiply));
   if (num1->value > num2->value) {
-    result.push_back(std::make_shared<Number>(num1, num2, OpType::Minus));
+    result.push_back(std::make_unique<Number>(num1, num2, OpType::Minus));
   } else {
-    result.push_back(std::make_shared<Number>(num2, num1, OpType::Minus));
+    result.push_back(std::make_unique<Number>(num2, num1, OpType::Minus));
   }
   if (num2->value != 0) {
-    result.push_back(std::make_shared<Number>(num1, num2, OpType::Divide));
+    result.push_back(std::make_unique<Number>(num1, num2, OpType::Divide));
   }
   if (num1->value != 0) {
-    result.push_back(std::make_shared<Number>(num2, num1, OpType::Divide));
+    result.push_back(std::make_unique<Number>(num2, num1, OpType::Divide));
   }
   return result;
 }
 
-std::shared_ptr<Number> calc(const std::vector<std::shared_ptr<Number>>& nums,
-                             const float target) {
+std::optional<std::string> calc(const std::vector<Number*>& nums, const float target) {
   const int N = nums.size();
   if (N == 1) {
     if (nums[0]->value == target) {
-      return nums[0];
+      return print(nums[0]);
     } else {
-      return nullptr;
+      return {};
     }
   }
 
-  std::vector<std::shared_ptr<Number>> reduced;
+  std::vector<Number*> reduced;
+  reduced.reserve(N-1);
   for (int i = 0; i < N; i++) {
     for (int j = i + 1; j < N; j++) {
       reduced.clear();
@@ -114,32 +114,34 @@ std::shared_ptr<Number> calc(const std::vector<std::shared_ptr<Number>>& nums,
         if (k == i || k == j) continue;
         reduced.push_back(nums[k]);
       }
-      for (std::shared_ptr<Number> num : combine(nums[i], nums[j])) {
-        reduced.push_back(num);
+      reduced.push_back(nullptr);
+      for (const auto& num : combine(nums[i], nums[j])) {
+        reduced[N-2] = num.get();
         const auto result = calc(reduced, target);
-        if (result != nullptr) {
+        if (result.has_value()) {
           return result;
         }
-        reduced.pop_back();
       }
     }
   }
-  return nullptr;
+  return {};
 }
 
 std::string calc24(const std::vector<int>& nums) {
-  std::vector<std::shared_ptr<Number>> numbers;
+  std::vector<std::unique_ptr<Number>> numbers;
+  std::vector<Number*> num_ptrs;
   std::string challenge;
   for (int i = 0; i < nums.size(); i++) {
     int x = nums[i];
-    numbers.emplace_back(std::make_shared<Number>(float(x)));
+    numbers.emplace_back(std::make_unique<Number>(float(x)));
+    num_ptrs.push_back(numbers[i].get());
     if (i > 0) challenge.append(", ");
     challenge.append(std::to_string(x));
   }
   challenge.append(" -> ");
-  const auto result = calc(numbers, 24);
-  if (result != nullptr) {
-    challenge.append(print(result));
+  const auto result = calc(num_ptrs, 24);
+  if (result.has_value()) {
+    challenge.append(result.value());
   } else {
     challenge.append("No Solution");
   }
