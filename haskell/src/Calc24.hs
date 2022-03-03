8441776e143b2dbd2e8e03681baf73fd1bc0ef57
Steven Chen <megakilo@gmail.com>
2022-03-02 16:43:13 -0800

    Add unit tests.

--- haskell/src/Calc24.hs
+++ haskell/src/Calc24.hs
@@ -1,4 +1,4 @@
-module Calc24 (main) where
+module Calc24 (main, calc24) where
 
 import Control.Monad (replicateM, replicateM_)
 import Data.List (find)
@@ -7,44 +7,46 @@ import System.Random (randomRIO)
 import Text.Printf (printf)
 
 data Number = Number
-  { value :: Float
-  , expression :: String
-  , operator :: Char
+  { value :: Float,
+    expression :: String,
+    operator :: Char
   }
 
 main :: IO ()
 main = replicateM_ 1000 $ do
   nums <- replicateM 4 $ randomRIO (1, 13 :: Integer)
-  putStrLn $ calc24 nums
+  putStrLn $ case calc24 nums of
+    Just result -> show nums ++ " -> " ++ result
+    Nothing -> show nums ++ " -> No Solution"
 
-calc24 :: [Integer] -> String
-calc24 nums = case calc numbers 24 of
-  Just result -> show nums ++ " -> " ++ result
-  Nothing     -> show nums ++ " -> No Solution"
-  where numbers = map (\x -> Number (fromInteger x) (show x) 'X') nums
+calc24 :: [Integer] -> Maybe String
+calc24 nums =
+  let numbers = map (\x -> Number (fromInteger x) (show x) 'X') nums
+   in calc numbers 24
 
 calc :: [Number] -> Float -> Maybe String
 calc [] _ = Nothing
 calc [x] target = if value x == target then Just (expression x) else Nothing
 calc nums target = fromMaybe Nothing (find isJust [calc xs target | xs <- reducedList])
-  where reducedList = split nums 2 >>= \([num1, num2], nontaken) -> [r : nontaken | r <- combine num1 num2]
+  where
+    reducedList = split nums 2 >>= \([num1, num2], nontaken) -> [r : nontaken | r <- combine num1 num2]
 
 split :: [a] -> Int -> [([a], [a])]
 split [] _ = []
 split xs 0 = [([], xs)]
-split xs@(h:t) n
+split xs@(h : t) n
   | length xs <= n = [(xs, [])]
   | otherwise =
-    map (\pair -> (fst pair, h : snd pair)) (split t n) ++
-    map (\pair -> (h : fst pair, snd pair)) (split t (n - 1))
+    map (\pair -> (fst pair, h : snd pair)) (split t n)
+      ++ map (\pair -> (h : fst pair, snd pair)) (split t (n - 1))
 
 combine :: Number -> Number -> [Number]
 combine (Number x xExpr xOp) (Number y yExpr yOp) =
-  [ Number (x + y) (printf "%s + %s" xExpr yExpr) '+'
-  , Number (x * y) (printf "%s * %s" (addParentheses xExpr xOp "+-") (addParentheses yExpr yOp "+-")) '*'
-  , Number (x / y) (printf "%s / %s" (addParentheses xExpr xOp "+-") (addParentheses yExpr yOp "+-*/")) '/'
-  , Number (y / x) (printf "%s / %s" (addParentheses yExpr yOp "+-") (addParentheses xExpr xOp "+-*/")) '/'
-  , if x > y
+  [ Number (x + y) (printf "%s + %s" xExpr yExpr) '+',
+    Number (x * y) (printf "%s * %s" (addParentheses xExpr xOp "+-") (addParentheses yExpr yOp "+-")) '*',
+    Number (x / y) (printf "%s / %s" (addParentheses xExpr xOp "+-") (addParentheses yExpr yOp "+-*/")) '/',
+    Number (y / x) (printf "%s / %s" (addParentheses yExpr yOp "+-") (addParentheses xExpr xOp "+-*/")) '/',
+    if x > y
       then Number (x - y) (printf "%s - %s" xExpr (addParentheses yExpr yOp "+-")) '-'
       else Number (y - x) (printf "%s - %s" yExpr (addParentheses xExpr xOp "+-")) '-'
   ]
--- haskell/test/Spec.hs
+++ haskell/test/Spec.hs
@@ -1,2 +1,26 @@
+import Calc24 (calc24)
+import Data.Maybe (fromMaybe)
+import qualified Language.Haskell.Interpreter as Hint
+import Test.Hspec
+
+evalResult :: Maybe String -> IO (Maybe Double)
+evalResult Nothing = return Nothing
+evalResult (Just s) = do
+  r <- Hint.runInterpreter $ do
+    Hint.setImports ["Prelude"]
+    Hint.interpret s (Hint.as :: Double)
+  return $ case r of
+    Left _ -> Nothing
+    Right x -> Just x
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "calc24" $ do
+    it "has valid solution for [2, 3, 4]." $ do
+      evalResult (calc24 [2, 3, 4]) `shouldReturn` Just 24
+    it "has no solution for [1, 3, 4]." $ do
+      evalResult (calc24 [1, 3, 4]) `shouldReturn` Nothing
+    it "has valid solution for [1, 2, 3, 4]." $ do
+      evalResult (calc24 [1, 2, 3, 4]) `shouldReturn` Just 24
+    it "has valid solution for [3, 3, 7, 7] with floating point calculation." $ do
+      evalResult (calc24 [3, 3, 7, 7]) `shouldReturn` Just 24
