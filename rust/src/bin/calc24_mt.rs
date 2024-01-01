4e57ebac8ca512f93b82190692f66635f590909d
Steven Chen <megakilo@gmail.com>
2023-12-31 22:23:42 -0800

    Use channel to pass result and avoid stdout lock overhead.

--- rust/src/bin/calc24_mt.rs
+++ rust/src/bin/calc24_mt.rs
@@ -1,29 +1,35 @@
 use std::sync::Arc;
 
 use calc24;
-use futures::future;
 use rand::Rng;
+use tokio::sync::mpsc;
 use tokio::task;
 
-async fn run(calculator: Arc<calc24::Calc24>, nums: [i32; 4]) {
-    match calculator.calc(nums) {
-        None => println!("{:?} -> No Solution", nums),
-        Some(answer) => println!("{:?} -> {}", nums, answer),
-    }
+async fn run(calculator: Arc<calc24::Calc24>, nums: [i32; 4], tx: mpsc::Sender<String>) {
+    let answer = match calculator.calc(nums) {
+        None => format!("{:?} -> No Solution", nums),
+        Some(r) => format!("{:?} -> {}", nums, r),
+    };
+    let _ = tx.send(answer).await;
 }
 
 #[tokio::main(flavor = "multi_thread")]
 async fn main() {
     let mut rng = rand::thread_rng();
     const N: usize = 4;
+    const TOTAL: usize = 100_000;
 
     let calculator: Arc<calc24::Calc24> = Arc::new(calc24::Calc24::new(N));
     let mut nums = [0; N];
-    let handles = (0..100000)
-        .map(|_| {
-            (0..N).for_each(|i| nums[i] = rng.gen_range(1..=13));
-            return task::spawn(run(calculator.clone(), nums));
-        })
-        .collect::<Vec<task::JoinHandle<()>>>();
-    future::join_all(handles).await;
+    let (tx, mut rx) = mpsc::channel::<String>(100);
+    (0..TOTAL).for_each(|_| {
+        (0..N).for_each(|i| nums[i] = rng.gen_range(1..=13));
+        task::spawn(run(calculator.clone(), nums, tx.clone()));
+    });
+    for _ in 0..TOTAL {
+        match rx.recv().await {
+            None => println!("Unexpected"),
+            Some(answer) => println!("{}", answer),
+        };
+    }
 }
