05c4c6bb7d05496aa72a8139b8e22de7e8074185
Steven Chen <megakilo@gmail.com>
2024-01-01 15:12:21 -0800

    Change to inline async block.

--- rust/src/bin/calc24_mt.rs
+++ rust/src/bin/calc24_mt.rs
@@ -5,18 +5,6 @@ use rand::Rng;
 use tokio::sync::mpsc;
 use tokio::task;
 
-async fn run<const N: usize>(
-    calculator: Arc<calc24::Calc24<N>>,
-    nums: [i32; N],
-    tx: mpsc::Sender<String>,
-) {
-    let answer = match calculator.calc(nums) {
-        None => format!("{:?} -> No Solution", nums),
-        Some(r) => format!("{:?} -> {}", nums, r),
-    };
-    let _ = tx.send(answer).await;
-}
-
 #[tokio::main(flavor = "multi_thread")]
 async fn main() {
     let mut rng = rand::thread_rng();
@@ -28,7 +16,15 @@ async fn main() {
     let (tx, mut rx) = mpsc::channel::<String>(100);
     (0..TOTAL).for_each(|_| {
         (0..N).for_each(|i| nums[i] = rng.gen_range(1..=13));
-        task::spawn(run(calculator.clone(), nums, tx.clone()));
+        let calculator = calculator.clone();
+        let tx = tx.clone();
+        task::spawn(async move {
+            let answer = match calculator.calc(nums) {
+                None => format!("{:?} -> No Solution", nums),
+                Some(r) => format!("{:?} -> {}", nums, r),
+            };
+            let _ = tx.send(answer).await;
+        });
     });
     for _ in 0..TOTAL {
         match rx.recv().await {
