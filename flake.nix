4f9ccc8af9174180ef3049394e5e06564eebe68c
Steven Chen <megakilo@gmail.com>
2024-01-16 20:56:11 -0800

    Add nix flake.

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "calc24";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        {
+          devShells.default =
+            pkgs.mkShell {
+              buildInputs = with pkgs; [
+                clang
+                git
+                pypy3
+                (haskellPackages.ghcWithPackages (pkgs: [ cabal-install ]))
+                go
+                cargo
+                rustc
+                rustfmt
+              ];
+
+              shellHook = ''
+                export PS1='[\[\033[1;32m\]calc24\[\033[0m\]:\[\033[1;34m\]\w\[\033[0m\]]\$ '
+              '';
+
+              RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+            };
+        }
+      );
+}
