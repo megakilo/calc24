7bb6cb79d95e787459780f7cddccfc3fa617c5b3
Steven Chen <megakilo@gmail.com>
2024-12-04 22:38:16 -0800

    Use fixed size pool for Operand.

--- zig/calc24.zig
+++ zig/calc24.zig
@@ -8,86 +8,42 @@ const OpType = enum {
     Divide,
 };
 
-const Operand = struct {
+fn List(comptime T: anytype, comptime N: usize) type {
+    return struct {
+        entries: [N]T = undefined,
+        count: usize = 0,
+
+        fn add(self: *@This(), item: T) usize {
+            const current = self.count;
+            self.entries[current] = item;
+            self.count += 1;
+            return current;
+        }
+
+        fn get(self: *const @This(), i: usize) *const T {
+            return &self.entries[i];
+        }
+    };
+}
+
+const Node = struct {
     index: usize = 0,
     op: OpType = .None,
-    left: ?*Operand = null,
-    right: ?*Operand = null,
-
-    pub fn eval(self: @This(), nums: []u8) f64 {
-        return switch (self.op) {
-            .None => @floatFromInt(nums[self.index]),
-            .Add => self.left.?.eval(nums) + self.right.?.eval(nums),
-            .Subtract => self.left.?.eval(nums) - self.right.?.eval(nums),
-            .Multiply => self.left.?.eval(nums) * self.right.?.eval(nums),
-            .Divide => self.left.?.eval(nums) / self.right.?.eval(nums),
-        };
-    }
-
-    pub fn toString(
-        self: @This(),
-        nums: []u8,
-        alloc: std.mem.Allocator,
-    ) !std.ArrayList(u8) {
-        var result = std.ArrayList(u8).init(alloc);
-        switch (self.op) {
-            .None => try result.writer().print("{d}", .{nums[self.index]}),
-            .Add => {
-                const l = try self.left.?.toString(nums, alloc);
-                const r = try self.right.?.toString(nums, alloc);
-                try result.writer().print("{s} + {s}", .{ l.items, r.items });
-            },
-            .Subtract => {
-                const l = try self.left.?.toString(nums, alloc);
-                const r = try self.right.?.toStringWrapped(nums, false, alloc);
-                try result.writer().print("{s} - {s}", .{ l.items, r.items });
-            },
-            .Multiply => {
-                const l = try self.left.?.toStringWrapped(nums, false, alloc);
-                const r = try self.right.?.toStringWrapped(nums, false, alloc);
-                try result.writer().print("{s} * {s}", .{ l.items, r.items });
-            },
-            .Divide => {
-                const l = try self.left.?.toStringWrapped(nums, false, alloc);
-                const r = try self.right.?.toStringWrapped(nums, true, alloc);
-                try result.writer().print("{s} / {s}", .{ l.items, r.items });
-            },
-        }
-        return result;
-    }
-
-    pub fn toStringWrapped(
-        self: @This(),
-        nums: []u8,
-        is_denominator: bool,
-        alloc: std.mem.Allocator,
-    ) error{ WriteError, OutOfMemory }!std.ArrayList(u8) {
-        var result = std.ArrayList(u8).init(alloc);
-        const r = try self.toString(nums, alloc);
-        if (self.op == .Add or self.op == .Subtract or (self.op != .None and is_denominator)) {
-            try result.writer().print("({s})", .{r.items});
-        } else {
-            try result.appendSlice(r.items);
-        }
-        return result;
-    }
+    left: usize = 0,
+    right: usize = 0,
 };
 
-fn combine(op1: *Operand, op2: *Operand, alloc: std.mem.Allocator) ![6]*Operand {
-    const add = try alloc.create(Operand);
-    add.* = .{ .op = .Add, .left = op1, .right = op2 };
-    const mult = try alloc.create(Operand);
-    mult.* = .{ .op = .Multiply, .left = op1, .right = op2 };
-    const sub1 = try alloc.create(Operand);
-    sub1.* = .{ .op = .Subtract, .left = op1, .right = op2 };
-    const sub2 = try alloc.create(Operand);
-    sub2.* = .{ .op = .Subtract, .left = op2, .right = op1 };
-    const div1 = try alloc.create(Operand);
-    div1.* = .{ .op = .Divide, .left = op1, .right = op2 };
-    const div2 = try alloc.create(Operand);
-    div2.* = .{ .op = .Divide, .left = op2, .right = op1 };
+const Index = usize;
+const Pool = List(Node, 5000);
 
-    return [_]*Operand{ add, mult, sub1, sub2, div1, div2 };
+fn reduce(op1: Index, op2: Index, pool: *Pool) [6]Index {
+    const add = pool.add(.{ .op = .Add, .left = op1, .right = op2 });
+    const mult = pool.add(.{ .op = .Multiply, .left = op1, .right = op2 });
+    const sub1 = pool.add(.{ .op = .Subtract, .left = op1, .right = op2 });
+    const sub2 = pool.add(.{ .op = .Subtract, .left = op2, .right = op1 });
+    const div1 = pool.add(.{ .op = .Divide, .left = op1, .right = op2 });
+    const div2 = pool.add(.{ .op = .Divide, .left = op2, .right = op1 });
+    return [_]Index{ add, mult, sub1, sub2, div1, div2 };
 }
 
 fn resultCount(comptime N: u8) u32 {
@@ -98,30 +54,14 @@ fn resultCount(comptime N: u8) u32 {
     }
 }
 
-fn List(comptime T: anytype, comptime N: usize) type {
-    return struct {
-        entries: [N]T = undefined,
-        count: usize = 0,
-
-        fn add(self: *@This(), item: T) void {
-            self.entries[self.count] = item;
-            self.count += 1;
-        }
-
-        fn get(self: *@This(), i: usize) T {
-            return self.entries[i];
-        }
-    };
-}
-
-fn generateFormula(comptime N: u8, indexes: []*Operand, alloc: std.mem.Allocator) ![resultCount(N)]*Operand {
-    var result: [resultCount(N)]*Operand = undefined;
+fn generate(comptime N: u8, indexes: []Index, pool: *Pool) [resultCount(N)]Index {
+    var result: [resultCount(N)]Index = undefined;
     var r: usize = 0;
     if (N == 1) {
         result[0] = indexes[0];
         return result;
     }
-    var reduced: [N - 1]*Operand = undefined;
+    var reduced: [N - 1]Index = undefined;
     for (0..N) |i| {
         for ((i + 1)..N) |j| {
             var m: u8 = 0;
@@ -131,9 +71,9 @@ fn generateFormula(comptime N: u8, indexes: []*Operand, alloc: std.mem.Allocator
                     m += 1;
                 }
             }
-            for (try combine(indexes[i], indexes[j], alloc)) |c| {
+            for (reduce(indexes[i], indexes[j], pool)) |c| {
                 reduced[N - 2] = c;
-                const roots = try generateFormula(N - 1, &reduced, alloc);
+                const roots = generate(N - 1, &reduced, pool);
                 for (roots) |root| {
                     result[r] = root;
                     r += 1;
@@ -146,28 +86,80 @@ fn generateFormula(comptime N: u8, indexes: []*Operand, alloc: std.mem.Allocator
 
 fn Calc24(comptime N: u8) type {
     return struct {
-        formula: [resultCount(N)]*Operand,
-        allocator: std.mem.Allocator,
+        formula: [resultCount(N)]Index = undefined,
+        pool: Pool = undefined,
 
-        pub fn init(alloc: std.mem.Allocator) !Calc24(N) {
-            var indexes: [N]*Operand = undefined;
+        pub fn init() Calc24(N) {
+            var c = Calc24(N){};
+            var indexes: [N]Index = undefined;
             for (0..N) |i| {
-                const x = try alloc.create(Operand);
-                x.* = .{ .index = i, .op = .None };
-                indexes[i] = x;
+                indexes[i] = c.pool.add(.{ .index = i, .op = .None });
             }
-            return .{ .allocator = alloc, .formula = try generateFormula(N, &indexes, alloc) };
+            c.formula = generate(N, &indexes, &c.pool);
+            return c;
         }
 
-        pub fn calc(self: @This(), nums: []u8) !std.ArrayList(u8) {
+        pub fn calc(self: *const @This(), nums: []u8, aa: std.mem.Allocator) !std.ArrayList(u8) {
             const target: f64 = 24;
-            var result = std.ArrayList(u8).init(self.allocator);
+            var result = std.ArrayList(u8).init(aa);
             for (self.formula) |f| {
-                if (f.eval(nums) == target) {
-                    return f.toString(nums, self.allocator);
+                if (self.eval(nums, f) == target) {
+                    return self.toString(nums, f, aa);
                 }
             }
             try result.appendSlice("No Solution");
+            return result;
+        }
+
+        fn eval(self: *const @This(), nums: []u8, index: Index) f64 {
+            const node = self.pool.get(index);
+            return switch (node.op) {
+                .None => @floatFromInt(nums[node.index]),
+                .Add => self.eval(nums, node.left) + self.eval(nums, node.right),
+                .Subtract => self.eval(nums, node.left) - self.eval(nums, node.right),
+                .Multiply => self.eval(nums, node.left) * self.eval(nums, node.right),
+                .Divide => self.eval(nums, node.left) / self.eval(nums, node.right),
+            };
+        }
+
+        fn toString(self: *const @This(), nums: []u8, index: Index, alloc: std.mem.Allocator) !std.ArrayList(u8) {
+            var result = std.ArrayList(u8).init(alloc);
+            const node = self.pool.get(index);
+            switch (node.op) {
+                .None => try result.writer().print("{d}", .{nums[node.index]}),
+                .Add => {
+                    const l = try self.toString(nums, node.left, alloc);
+                    const r = try self.toString(nums, node.right, alloc);
+                    try result.writer().print("{s} + {s}", .{ l.items, r.items });
+                },
+                .Subtract => {
+                    const l = try self.toString(nums, node.left, alloc);
+                    const r = try self.toStringWrapped(nums, node.right, alloc, false);
+                    try result.writer().print("{s} - {s}", .{ l.items, r.items });
+                },
+                .Multiply => {
+                    const l = try self.toStringWrapped(nums, node.left, alloc, false);
+                    const r = try self.toStringWrapped(nums, node.right, alloc, false);
+                    try result.writer().print("{s} * {s}", .{ l.items, r.items });
+                },
+                .Divide => {
+                    const l = try self.toStringWrapped(nums, node.left, alloc, false);
+                    const r = try self.toStringWrapped(nums, node.right, alloc, true);
+                    try result.writer().print("{s} / {s}", .{ l.items, r.items });
+                },
+            }
+            return result;
+        }
+
+        fn toStringWrapped(self: *const @This(), nums: []u8, index: Index, alloc: std.mem.Allocator, is_denominator: bool) error{ WriteError, OutOfMemory }!std.ArrayList(u8) {
+            var result = std.ArrayList(u8).init(alloc);
+            const r = try self.toString(nums, index, alloc);
+            const node = self.pool.get(index);
+            if (node.op == .Add or node.op == .Subtract or (node.op != .None and is_denominator)) {
+                try result.writer().print("({s})", .{r.items});
+            } else {
+                try result.appendSlice(r.items);
+            }
             return result;
         }
     };
@@ -191,8 +183,8 @@ pub fn main() !void {
     const N: u8 = 4;
     var nums: [N]u8 = undefined;
     var challenge = std.ArrayList(u8).init(aa);
-    const calc = try Calc24(N).init(aa);
-    for (0..1) |_| {
+    const calc = Calc24(N).init();
+    for (0..100000) |_| {
         for (0..N) |i| {
             nums[i] = rand.intRangeAtMost(u8, 1, 13);
             if (i > 0) {
@@ -200,13 +192,8 @@ pub fn main() !void {
             }
             try challenge.writer().print("{d}", .{nums[i]});
         }
-        const result = try calc.calc(&nums);
+        const result = try calc.calc(&nums, aa);
         try stdout.print("{s} -> {s}\n", .{ challenge.items, result.items });
         challenge.clearRetainingCapacity();
     }
-    var list = List(u8, 100){};
-    list.add('a');
-    list.add('b');
-    list.add('c');
-    std.debug.print("{c}\n", .{list.get(1)});
 }
